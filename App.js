4eeeb65ed70119fe866b36adadfd9fc5452f46dd
etigerstudio <etigerstudio@gmail.com>
2018-09-17 13:07:18 +0800

    Cleaned boilerplate App.js codes.

--- App.js
+++ App.js
@@ -13,15 +13,8 @@ import Cards from './components/cards'
 import AQIHeader from './components/aqi-header'
 import Caption from './components/caption'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
@@ -57,21 +50,3 @@ const styles = StyleSheet.create({
       resizeMode: 'cover',
   },
 });
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});*/
